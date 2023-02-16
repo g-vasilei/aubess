755a09a010d7dcd2c083cabefcd7d00ce98c5d79
g-vasilei <ram.kunya.9@gmail.com>
2023-02-16 20:33:53 +0200

    add some new products

--- src/Products/data.js
+++ src/Products/data.js
@@ -2,66 +2,167 @@ const data = {
    products: [
       {
          id: 1,
-         title: 'Tuya Zigbee Smart Temperature and Humidity Sensor',
-         description: `TZTH1 - Tuya Zigbee Smart Temperature and Humidity Sensor
+         title: 'Tuya WiFi Smart Magnetic Door/Window Sensor',
+         description: `The device with an elegant design has been studied so as not to disturb the internal environment in which it must be placed.
 
-            The TZTH1 works in Zigbee Protocol. It is powered by two batteries that provide a long battery life.
-            
-            It has functions to measure the current temperature and humidity and monitors the battery status of the device.
-            
-            View history record in App for your house temperature and humidity.  Monitor your house temperature and humidity no matter where you are.
-            
-            It can connect with Amazon Alexa and Google Assistant voice platform.`,
-         slug: 'TZTH1',
-         category: 'Smart DIY Switch',
-         category_slug: 'smart-diy-switch',
+         This smart door sensor is equipped with the latest designed technology with an emphasis on maximum reliability, simplicity and sensitivity.
+         
+         The wireless magnetic sensor on doors and windows detects unwanted intrusion when the sensor body and magnet move apart.
+         
+         Intelligent Real-time detection of the magnetic opening and closing of the door the device quickly sends the alarm information to the mobile phone.
+         
+         Using the mobile application you will immediately receive the information that has been detected a breach of a guarded entrance - doors or windows.
+         
+         Immediate response when attacking a protected object without delay (opening a window / door).`,
+         slug: 'TWMDS1',
+         category: 'Smart Sensor',
+         category_slug: 'smart-sensor',
          img: [
-            'https://www.aerial.net/shop/imageslarge/TZTH1_main.jpg',
-            'https://www.aerial.net/shop/imageslarge/TZTH1_front.jpg',
-            'https://www.aerial.net/shop/imageslarge/TZTH1_top.jpg',
-            'https://www.aerial.net/shop/imageslarge/TZTH1_bottom.jpg',
-            'https://www.aerial.net/shop/imageslarge/TZTH1_dims.jpg',
+            'https://www.aerial.net/shop/imageslarge/TWMS1_main.jpg',
+            'https://www.aerial.net/shop/imageslarge/TWMS1_front.jpg',
+            'https://www.aerial.net/shop/imageslarge/TWMS1_top.jpg',
+            'https://www.aerial.net/shop/imageslarge/TWMS1_mount.jpg',
+            'https://www.aerial.net/shop/imageslarge/TWMS1_dims.jpg',
          ],
-         seoTitle: 'TZTH1 | AUBESS.eu',
-         seoDescription: 'Tuya Zigbee Smart Temperature and Humidity Sensor',
+         seoTitle: 'TWMDS1 | AUBESS.eu',
+         seoDescription: 'Tuya WiFi Smart Magnetic Door/Window Sensor',
       },
       {
          id: 2,
-         title: 'Tuya Zigbee Smart Temperature and Humidity Sensor',
-         description: `TZTH1 - Tuya Zigbee Smart Temperature and Humidity Sensor
+         title: 'TWSW161G - Tuya WiFi 16A Smart Switch - 1 Channel, with Neutral',
+         description: `The WiFi TWSW161G Module is a small 1-circuit flush-mounted relay that will transform your switch traditional in a smart device that you can control with your smartphone.
 
-           The TZTH1 works in Zigbee Protocol. It is powered by two batteries that provide a long battery life.
-           
-           It has functions to measure the current temperature and humidity and monitors the battery status of the device.
-           
-           View history record in App for your house temperature and humidity.  Monitor your house temperature and humidity no matter where you are.
-           
-           It can connect with Amazon Alexa and Google Assistant voice platform.`,
-         slug: 'TZTH1',
-         category: 'Smart Plug',
-         category_slug: 'smart-plug',
+         The device works with traditional switches, and allows you to use both the smart function and that traditional , it also works with toggle switches.
+         
+         This smart DIY switch module can be used at home, office, hotel and other places.
+         
+         The application allows you to monitor the operation of the relay, even when using the traditional method. In the application, we can set a weekly work schedule for the device.
+         
+         The switch works with Google Home and Amazon Alexa voice assistants.`,
+         slug: 'TWSW161G',
+         category: 'Smart Switch',
+         category_slug: 'smart-diy-switch',
          img: [
-            'https://www.aerial.net/shop/imageslarge/TZTH1_main.jpg',
-            'https://www.aerial.net/shop/imageslarge/TZTH1_front.jpg',
-            'https://www.aerial.net/shop/imageslarge/TZTH1_top.jpg',
-            'https://www.aerial.net/shop/imageslarge/TZTH1_bottom.jpg',
-            'https://www.aerial.net/shop/imageslarge/TZTH1_dims.jpg',
+            'https://www.aerial.net/shop/imageslarge/TWSW161G_main.jpg',
+            'https://www.aerial.net/shop/imageslarge/TWSW161G_sides.jpg',
+            'https://www.aerial.net/shop/imageslarge/TWSW161G_connect.jpg',
+            'https://www.aerial.net/shop/imageslarge/TWSW161G_rear.jpg',
+            'https://www.aerial.net/shop/imageslarge/TWSW161G_dims.jpg',
          ],
-         seoTitle: 'TZTH1 | AUBESS.eu',
-         seoDescription: 'Tuya Zigbee Smart Temperature and Humidity Sensor',
+         seoTitle: 'TWSW161G | AUBESS.eu',
+         seoDescription: 'TWSW161G - Tuya WiFi 16A Smart Switch - 1 Channel, with Neutral',
       },
       {
          id: 3,
-         title: 'Tuya Zigbee Smart Temperature and Humidity Sensor',
+         title: 'TWTH1 - Tuya WiFi Smart Temperature and Humidity Sensor',
+         description: `TWΤΗ1 - Tuya WiFi Smart Temperature and Humidity Sensor
+
+         The TWΤΗ1 works in WiFi 2.4 GHz band. It is powered by two batteries that provide a long battery life.
+         
+         It has functions to measure the current temperature and humidity and monitors the battery status of the device.
+         
+         View history record in App for your house temperature and humidity.  Monitor your house temperature and humidity no matter where you are.
+         
+         It can connect with Amazon Alexa and Google Assistant voice platform.`,
+         slug: 'TWTH1',
+         category: 'Smart Sensor',
+         category_slug: 'smart-sensor',
+         img: [
+            'https://www.aerial.net/shop/imageslarge/TZTH1_main.jpg',
+            'https://www.aerial.net/shop/imageslarge/TWTH1_front.jpg',
+            'https://www.aerial.net/shop/imageslarge/TWTH1_top.jpg',
+            'https://www.aerial.net/shop/imageslarge/TZTH1_bottom.jpg',
+            'https://www.aerial.net/shop/imageslarge/TZTH1_dims.jpg',
+         ],
+         seoTitle: 'TWTH1 | AUBESS.eu',
+         seoDescription: 'TWTH1 - Tuya WiFi Smart Temperature and Humidity Sensor',
+      },
+      {
+         id: 4,
+         title: 'TWSW161P - Tuya WiFi 16A Smart Switch - 1 Channel, Energy Monitor',
+         description: `The WiFi TWSW161P Module is a small 1-circuit flush-mounted relay that will transform your switch traditional in a smart device that you can control with your smartphone.
+
+         The switch can record power usage in real time. You can view the monthly electricity consumption with the APP, which is convenient for you to make electricity consumption plans to reduce electricity bills.
+         
+         The device works with traditional switches, and allows you to use both the smart function and that traditional , it also works with toggle switches.
+         
+         The application allows you to monitor the operation of the relay, even when using the traditional method. In the application, we can set a weekly work schedule for the device.
+         
+         The switch works with Google Home and Amazon Alexa voice assistants.`,
+         slug: 'TWSW161P',
+         category: 'Smart Switch',
+         category_slug: 'smart-diy-switch',
+         img: [
+            'https://www.aerial.net/shop/imageslarge/TWSW161P_main.jpg',
+            'https://www.aerial.net/shop/imageslarge/TWSW161P_sides.jpg',
+            'https://www.aerial.net/shop/imageslarge/TWSW161P_connect.jpg',
+            'https://www.aerial.net/shop/imageslarge/TWSW161P_rear.jpg',
+            'https://www.aerial.net/shop/imageslarge/TWSW161P_dims.jpg',
+         ],
+         seoTitle: 'TWSW161P | AUBESS.eu',
+         seoDescription: 'TWSW161P - Tuya WiFi 16A Smart Switch - 1 Channel, Energy Monitor',
+      },
+      {
+         id: 5,
+         title: 'TWRC1 - Tuya WiFi Smart IR Remote Controller',
+         description: `TWRC1 - Tuya WiFi  Smart IR Remote Controller
+
+         Use your smart phone to control your IR home appliances. (compatible with most of brands)
+         
+         Supports 4000 brands, 50000 IR Code on cloud, supported devices library is constantly being updated by the cloud.
+         
+         It has LEARNING FUNCTON that allows you to duplicate functions of any remote control that is not supported by default .
+         
+         Works with Amazon Alexa, Google Assistant, Siri(iOS 12.0 or later), etc.`,
+         slug: 'TWRC1',
+         category: 'Gateway',
+         category_slug: 'gateway',
+         img: [
+            'https://www.aerial.net/shop/imageslarge/TWRC1_main.jpg',
+            'https://www.aerial.net/shop/imageslarge/TWRC1_3d.jpg',
+            'https://www.aerial.net/shop/imageslarge/TWRC1_top.jpg',
+            'https://www.aerial.net/shop/imageslarge/TWRC1_rear.jpg',
+            'https://www.aerial.net/shop/imageslarge/TWRC1_dims.jpg',
+         ],
+         seoTitle: 'TWRC1 | AUBESS.eu',
+         seoDescription: 'TWRC1 - Tuya WiFi Smart IR Remote Controller',
+      },
+      {
+         id: 6,
+         title: 'TZWG1 - Tuya Zigbee 3.0 Gateway, WiFi ZigBee Hub/Bridge',
+         description: `TZWG1 - Tuya Zigbee 3.0 Gateway, WiFi ZigBee Hub/Bridge - connects up to 30 ZigBee devices
+
+         ZigBee 3.0 Wireless Gateway WiFi ZigBee Hub Bridge 
+         
+         This Gateway ZigBee 3.0 WiFi 2.4G is a control unit that can be connected to all Smart ZigBee products (such as sensors, lights, switches etc. with a maximum of 30 units).
+         
+         Built-in Wi-Fi module and Zigbee module, so that ZigBee devices can be controlled via WiFi via the TUYA app or controlled locally via the ZigBee network.
+         
+         Use the smartphone to remotely control the entire Zigbee system. Excellent performance, stable and reliable with a slim design.`,
+         slug: 'TZWG1',
+         category: 'Gateway',
+         category_slug: 'gateway',
+         img: [
+            'https://www.aerial.net/shop/imageslarge/TZWG1_main.jpg',
+            'https://www.aerial.net/shop/imageslarge/TZWG1_3d.jpg',
+            'https://www.aerial.net/shop/imageslarge/TZWG1_top.jpg',
+            'https://www.aerial.net/shop/imageslarge/TZWG1_dexc.jpg',
+         ],
+         seoTitle: 'TZWG1 | AUBESS.eu',
+         seoDescription: 'TZWG1 - Tuya Zigbee 3.0 Gateway, WiFi ZigBee Hub/Bridge',
+      },
+      {
+         id: 7,
+         title: 'TZTH1 - Tuya Zigbee Smart Temperature and Humidity Sensor',
          description: `TZTH1 - Tuya Zigbee Smart Temperature and Humidity Sensor
 
-           The TZTH1 works in Zigbee Protocol. It is powered by two batteries that provide a long battery life.
-           
-           It has functions to measure the current temperature and humidity and monitors the battery status of the device.
-           
-           View history record in App for your house temperature and humidity.  Monitor your house temperature and humidity no matter where you are.
-           
-           It can connect with Amazon Alexa and Google Assistant voice platform.`,
+         The TZTH1 works in Zigbee Protocol. It is powered by two batteries that provide a long battery life.
+         
+         It has functions to measure the current temperature and humidity and monitors the battery status of the device.
+         
+         View history record in App for your house temperature and humidity.  Monitor your house temperature and humidity no matter where you are.
+         
+         It can connect with Amazon Alexa and Google Assistant voice platform.`,
          slug: 'TZTH1',
          category: 'Smart Sensor',
          category_slug: 'smart-sensor',
@@ -73,57 +174,58 @@ const data = {
             'https://www.aerial.net/shop/imageslarge/TZTH1_dims.jpg',
          ],
          seoTitle: 'TZTH1 | AUBESS.eu',
-         seoDescription: 'Tuya Zigbee Smart Temperature and Humidity Sensor',
+         seoDescription: 'TZTH1 - Tuya Zigbee Smart Temperature and Humidity Sensor',
       },
       {
-         id: 4,
-         title: 'Tuya Zigbee Smart Temperature and Humidity Sensor',
-         description: `TZTH1 - Tuya Zigbee Smart Temperature and Humidity Sensor
+         id: 8,
+         title: 'TZSW101G - Tuya ZigBee 10A Smart Switch - 1 Channel, with Neutral',
+         description: `The Zigbee TZSW101G Module is a small 1-circuit flush-mounted relay that will transform your switch traditional in a smart device that you can control with your smartphone.
 
-           The TZTH1 works in Zigbee Protocol. It is powered by two batteries that provide a long battery life.
-           
-           It has functions to measure the current temperature and humidity and monitors the battery status of the device.
-           
-           View history record in App for your house temperature and humidity.  Monitor your house temperature and humidity no matter where you are.
-           
-           It can connect with Amazon Alexa and Google Assistant voice platform.`,
-         slug: 'TZTH1',
+         The device works with traditional switches, and allows you to use both the smart function and that traditional , it also works with toggle switches.
+         
+         This smart DIY switch module can be used at home, office, hotel and other places.
+         
+         The application allows you to monitor the operation of the relay, even when using the traditional method. In the application, we can set a weekly work schedule for the device.
+         
+         The switch works with Google Home and Amazon Alexa voice assistants.
+         
+         A ZIGBEE GATEWAY and the Tuya Smart or Smart Life application are required for operation. `,
+         slug: 'TZSW101G',
          category: 'Smart Switch',
-         category_slug: 'smart-switch',
+         category_slug: 'smart-diy-switch',
          img: [
-            'https://www.aerial.net/shop/imageslarge/TZTH1_main.jpg',
-            'https://www.aerial.net/shop/imageslarge/TZTH1_front.jpg',
-            'https://www.aerial.net/shop/imageslarge/TZTH1_top.jpg',
-            'https://www.aerial.net/shop/imageslarge/TZTH1_bottom.jpg',
-            'https://www.aerial.net/shop/imageslarge/TZTH1_dims.jpg',
+            'https://www.aerial.net/shop/imageslarge/TZSW101G_main.jpg',
+            'https://www.aerial.net/shop/imageslarge/TZSW101G_sides.jpg',
+            'https://www.aerial.net/shop/imageslarge/TZSW101G_connect.jpg',
+            'https://www.aerial.net/shop/imageslarge/TZSW101G_rear.jpg',
+            'https://www.aerial.net/shop/imageslarge/TZSW101G_dims.jpg',
          ],
-         seoTitle: 'TZTH1 | AUBESS.eu',
-         seoDescription: 'Tuya Zigbee Smart Temperature and Humidity Sensor',
+         seoTitle: 'TZSW101G | AUBESS.eu',
+         seoDescription: 'TZSW101G - Tuya ZigBee 10A Smart Switch - 1 Channel, with Neutral',
       },
       {
-         id: 5,
-         title: 'Tuya Zigbee Smart Temperature and Humidity Sensor',
-         description: `TZTH1 - Tuya Zigbee Smart Temperature and Humidity Sensor
+         id: 9,
+         title: 'TZWG1 - Tuya Zigbee 3.0 Gateway, WiFi ZigBee Hub/Bridge',
+         description: `TZWG1 - Tuya Zigbee 3.0 Gateway, WiFi ZigBee Hub/Bridge - connects up to 30 ZigBee devices
 
-           The TZTH1 works in Zigbee Protocol. It is powered by two batteries that provide a long battery life.
-           
-           It has functions to measure the current temperature and humidity and monitors the battery status of the device.
-           
-           View history record in App for your house temperature and humidity.  Monitor your house temperature and humidity no matter where you are.
-           
-           It can connect with Amazon Alexa and Google Assistant voice platform.`,
-         slug: 'TZTH1',
+         ZigBee 3.0 Wireless Gateway WiFi ZigBee Hub Bridge 
+         
+         This Gateway ZigBee 3.0 WiFi 2.4G is a control unit that can be connected to all Smart ZigBee products (such as sensors, lights, switches etc. with a maximum of 30 units).
+         
+         Built-in Wi-Fi module and Zigbee module, so that ZigBee devices can be controlled via WiFi via the TUYA app or controlled locally via the ZigBee network.
+         
+         Use the smartphone to remotely control the entire Zigbee system. Excellent performance, stable and reliable with a slim design.`,
+         slug: 'TZWG1',
          category: 'Gateway',
          category_slug: 'gateway',
          img: [
-            'https://www.aerial.net/shop/imageslarge/TZTH1_main.jpg',
-            'https://www.aerial.net/shop/imageslarge/TZTH1_front.jpg',
-            'https://www.aerial.net/shop/imageslarge/TZTH1_top.jpg',
-            'https://www.aerial.net/shop/imageslarge/TZTH1_bottom.jpg',
-            'https://www.aerial.net/shop/imageslarge/TZTH1_dims.jpg',
+            'https://www.aerial.net/shop/imageslarge/TZWG1_main.jpg',
+            'https://www.aerial.net/shop/imageslarge/TZWG1_3d.jpg',
+            'https://www.aerial.net/shop/imageslarge/TZWG1_top.jpg',
+            'https://www.aerial.net/shop/imageslarge/TZWG1_dexc.jpg',
          ],
-         seoTitle: 'TZTH1 | AUBESS.eu',
-         seoDescription: 'Tuya Zigbee Smart Temperature and Humidity Sensor',
+         seoTitle: 'TZSW101G | AUBESS.eu',
+         seoDescription: 'TZSW101G - Tuya ZigBee 10A Smart Switch - 1 Channel, with Neutral',
       },
    ],
 }
