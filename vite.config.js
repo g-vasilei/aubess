c7e34d4feb0e4c9ffecb1801828c4484d20ac695
g-vasilei <ram.kunya.9@gmail.com>
2023-02-17 17:52:45 +0200

    url preview 9

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { ssr } from 'vite-plugin-ssr/plugin'
+import viteSSR from 'vite-ssr/plugin.js'
 
 // https://vitejs.dev/config/
 export default defineConfig({
